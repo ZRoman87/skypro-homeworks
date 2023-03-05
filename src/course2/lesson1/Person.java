25508300ce7b5153e30a0b00db7eab49db36bbba
ZRoman87 <zrazhevskiy.roman@yandex.ru>
2023-03-05 21:53:31 +0300

    Fix issues after mentor review - Lesson 1 home work

---
+++ src/course2/lesson1/Person.java
@@ -0,0 +1,14 @@
+package course2.lesson1;
+
+public class Person {
+    String name;
+    String surname;
+
+    @Override
+    public String toString() {
+        return "Person{" +
+                "name='" + name + '\'' +
+                ", surname='" + surname + '\'' +
+                '}';
+    }
+}
